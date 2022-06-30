cf1a4304b72c8b23a30ca2a6b465c2670315da93
engrkashem <kashemaust@gmail.com>
2022-06-30 20:12:59 +0600

    login form added design only

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,9 +1,86 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from "react-router-dom";
 
 const Login = ({ setAuth }) => {
+    const { register, formState: { errors }, handleSubmit, getValues, reset } = useForm();
+
+    const onSubmit = data => {
+        console.log(data);
+
+    };
+
     return (
-        <div>
-            <h2>Login</h2>
+        <div className=' flex h-screen justify-center items-center bg-cover'>
+            <div className="card w-96 lg:w-1/2 bg-base-100 shadow-xl glass">
+                <div className="card-body items-center">
+                    <h2 className="card-title">LOGIN</h2>
+                    <form onSubmit={handleSubmit(onSubmit)} className="w-full ">
+
+                        <div className="form-control ">
+                            <label className="label">
+                                <span
+                                    className="label-text">Email
+                                </span>
+                            </label>
+                            <input
+                                type="email"
+                                placeholder="Your Email"
+                                className="input input-bordered w-full "
+                                {...register("email", {
+                                    required: {
+                                        value: true,
+                                        message: 'Email is Required'
+                                    },
+                                    pattern: {
+                                        value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                                        message: 'Enter a valid Email'
+                                    }
+                                })}
+                            />
+                            <label className="label">
+                                {errors.email?.type === 'required' && <span
+                                    className="label-text-alt text-rose-500">{errors.email.message}
+                                </span>}
+                                {errors.email?.type === 'pattern' && <span
+                                    className="label-text-alt text-rose-500">{errors.email.message}
+                                </span>}
+                            </label>
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span
+                                    className="label-text">Password
+                                </span>
+                            </label>
+                            <input
+                                type="password"
+                                placeholder="Your Password" className="input input-bordered w-full"
+                                {...register("password", {
+                                    required: {
+                                        value: true,
+                                        message: 'Password is Required'
+                                    },
+                                    pattern: {
+                                        value: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
+                                        message: 'Password must contains at least one upper case, one lower case, one digit, one special character and minimum length 8 characters '
+                                    }
+                                })}
+                            />
+                            <label className="label">
+                                {errors.password?.type === 'required' && <span
+                                    className="label-text-alt text-rose-500">{errors.password.message}
+                                </span>}
+                                {errors.password?.type === 'pattern' && <span
+                                    className="label-text-alt text-rose-500">{errors.password.message}
+                                </span>}
+                            </label>
+                        </div>
+                        <input className='btn btn-outline btn-primary w-1/2 mx-auto block' type="submit" value='LOGIN' />
+                    </form>
+                    <p className=' font-semibold mt-5'>New to SKS Inc.? <Link to={'/register'} className=' text-primary text-xl'>Register</Link></p>
+                </div>
+            </div>
         </div>
     );
 };
